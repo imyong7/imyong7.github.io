5321c271d59e8fb187aac241909ab8f488e29c91
Lim Ho Yong <imyong7@hanmail.net>
2021-11-03 01:04:03 +0900

    apply svelte

--- rollup.config.js
+++ rollup.config.js
@@ -5,7 +5,7 @@ import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
 import css from 'rollup-plugin-css-only';
 
-const production = process.env.ROLLUP_WATCH;
+const production = !process.env.ROLLUP_WATCH;
 
 function serve() {
 	let server;
@@ -45,7 +45,7 @@ export default {
 		}),
 		// we'll extract any component CSS out into
 		// a separate file - better for performance
-		css({ output: 'bundle.css' }),
+		css({ output: 'build/bundle.css' }),
 
 		// If you have external dependencies installed from
 		// npm, you'll most likely need these plugins. In
